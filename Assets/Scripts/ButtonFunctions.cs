71d4c87cfb59c8f5cf80bd795374412a21533e77
ivaylolivanov <ivaylolivanov95@gmail.com>
2022-12-15 19:22:30 +0200

    Add guardin check
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/ButtonFunctions.cs
+++ Assets/Scripts/ButtonFunctions.cs
@@ -14,6 +14,8 @@ public class ButtonFunctions : MonoBehaviour {
     }
 
     void Update() {
+        if(game == null) return;
+
         if(game.GetRating() <= 0) {
             LoadEndGameScene();
         }
